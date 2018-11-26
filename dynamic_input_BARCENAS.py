3132a4d361dab78d2fa4ea0cc3ef36d1b709b57d
kmbars <45374643+kmbars@users.noreply.github.com>
2018-11-27 07:51:54 +0800

    Create dynamic_input_BARCENAS.py

---
+++ dynamic_input_BARCENAS.py
@@ -0,0 +1,43 @@
+CCS 1A Programming 2
+CCS 1A
+Due Nov 20, 12:00 PM
+Challenge: Caesar Cipher
+303030 points out of possible 30
+
+Haisoj Nayahub
+Nov 20
+Using the pattern of Caesar Cipher, where A=B, B=C, C=D ....
+
+Write a python code capable of conversion, with dynamic input.
+
+Point System:
+
+10 pts for input
+20 pts for algorithm
+
+Total of: 30 pts
+This will serve as grade for Quiz. (LA)
+Your work
+Graded
+
+gene_decoding_BARCENAS.1.py
+Text
+
+dynamic_input_BARCENAS.py
+Text
+Challenge: Caesar Cipher
+#accept input from user
+name = input ("Enter your Name: ")
+print ("Your Name is: " + name)
+
+
+#accepts numerical input
+fnum = int (input ("Enter First Number: "))
+snum = int (input ("Enter Second Number: "))
+
+to_int_value = int ("3")
+
+print (fnum + snum)
+print (to_int_value ** 3)
+dynamic_input_BARCENAS.py
+Displaying dynamic_input_BARCENAS.py.

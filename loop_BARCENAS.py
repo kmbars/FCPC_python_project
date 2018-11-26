d6bed70ed6eb8d6ed3d2df462fdb4a7e2a2e6f6c
kmbars <45374643+kmbars@users.noreply.github.com>
2018-11-27 07:50:25 +0800

    loop_BARCENAS.py

---
+++ loop_BARCENAS.py
@@ -0,0 +1,37 @@
+CCS 1A Programming 2
+CCS 1A
+Due Nov 20, 10:30 AM
+Upload Source Code -- Using For Loops
+
+Haisoj Nayahub
+Nov 20
+Either upload one source code (combined) or two sources (separate) applying the For-loops:
+1. Series for 2,3,4, and 12
+2. Multiplication Table (10-20)
+Your work
+Turned in
+
+loop_BARCENAS.py
+Text
+Upload Source Code -- Using For Loops
+#loop by 2
+for x in range (2,12,2):
+    print (x)
+#loop by 3
+for x in range (3,15,3):
+    print (x)
+#loop by 4
+for x in range (4,24,4):
+    print (x)
+#loop by 12
+for x in range (12,48,12):
+    print (x)
+
+#nested loop sample
+
+for y in range (10,21):
+    for z in range (1,11):
+        print (z*y,end='\t')
+    print ()
+loop_BARCENAS.py
+Displaying loop_BARCENAS.py.

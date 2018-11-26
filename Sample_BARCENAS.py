1d65b9834986da3478bcdf2bc3936c0011badd7e
kmbars <45374643+kmbars@users.noreply.github.com>
2018-11-27 07:51:05 +0800

    Create Sample_BARCENAS.py

---
+++ Sample_BARCENAS.py
@@ -0,0 +1,56 @@
+CCS 1A Programming 2
+CCS 1A
+Due Nov 20, 9:10 AM
+Upload Source Code -- Sample code from Declaration to If-Else-Else If
+
+Haisoj Nayahub
+Nov 20
+Code
+Your work
+Turned in late
+
+Sample_BARCENAS.py
+Text
+Private comments
+
+Kirsten BarcenasNov 20
+LEGEND AKO
+
+Name = "Km The Legend"
+Age = "12"
+Location = "Philippines"
+
+print (Name)
+print (Age)
+print (Location)
+
+#Numerical Chuchu
+
+Sum = " 1 + 100 "
+Diff = " 100 - 12 "
+Product = "10 * 5 "
+Quotient = " 10 / 2 "
+
+print (Sum)
+print (Diff)
+print (Product)
+print (Quotient)
+
+#Arithnetic
+
+division = 100 / 10
+subtraction = 90 - 70
+modulus = 100 % 10
+
+print("division")
+print("subtraction")
+print("modulus")
+
+if division > subtraction:
+    print("mahirap to")
+elif subtraction > modulus:
+    print("eto yung tamang sagot")
+else:
+    print("km the LEGEND")
+Sample_BARCENAS.py
+Displaying Sample_BARCENAS.py.
